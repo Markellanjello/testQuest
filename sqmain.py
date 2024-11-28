f0d57e2245a170a85100f0a15af58e8bb0ece3a3
Markel <166810997+Markellanjello@users.noreply.github.com>
2024-11-28 10:09:54 +0300

    Update sqmain.py

--- sqmain.py
+++ sqmain.py
@@ -22,18 +22,18 @@ def main():
         elif command == "edit":
             pd_show(all_data)
             index = input("Введите индекс строки, которую хотите редактировать, для выхода введите exit: ")
-            if index == "exit": return
+            if index == "exit": continue
             data = cur.execute(f"SELECT * FROM directory WHERE id={int(index)}").fetchone()
             person = input(f"Вы собираетесь редактировать - {data} Введите данные через запятую, в том же формате: ").split(", ")
             cur.execute(f"UPDATE directory SET surname='{person[0]}', name='{person[1]}', father_name='{person[2]}', org_name='{person[3]}', phone_pub='{person[4]}', work_phone='{person[5]}' WHERE id={index}")
         elif command == "dump":
             person = str(input("Введите данные через запятую, в таком порядке - 'фамилия', 'имя', 'отчество', 'название организации', 'телефон личный (сотовый)', 'телефон рабочий', для выхода введите exit: ")).split(", ")
-            if person[0] == "exit": return
+            if person[0] == "exit": continue
             cur.execute(f"INSERT INTO directory (surname, name, father_name, org_name, phone_pub, work_phone) VALUES ('{person[0]}', '{person[1]}', '{person[2]}', '{person[3]}', '{person[4]}', '{person[5]}')")
         elif command == "delete":
             pd_show(all_data)
             index = input("Введите индекс строки, которую хотите удалить, для выхода введите exit: ")
-            if index == "exit": return
+            if index == "exit": continue
             cur.execute(f"DELETE FROM directory WHERE id={int(index)}")
         elif command == "search":
             search = input("Введите данные для поиска через запятую например - Иванов, 8991411414141: ").split(", ")
